c2e14abfb3345f2f3e2f189ee28e181208f090a1
Toufu <hamm157famm7423@gmail.com>
2024-07-04 21:42:20 +0900

    Updater: Change package name
    Change-Id: Ia5cf374794886c601fc440a9fba77bede6fddbd7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "tokyo.witaqua.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
