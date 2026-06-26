f0ecb135c9866496fc255d02bd25a4313a35b847
Toufu <hamm157famm7423@gmail.com>
2026-06-26 15:24:30 +0000

    Updater: Adapt to WitAqua
    Change-Id: I42a77336895c51d1a15ddf9d012cbd0f6e7caf99

--- app/src/main/java/org/lineageos/updater/deviceinfo/DeviceInfoUtils.kt
+++ app/src/main/java/org/lineageos/updater/deviceinfo/DeviceInfoUtils.kt
@@ -14,11 +14,10 @@ object DeviceInfoUtils : SettingsLibDeviceInfoUtils() {
     private const val PROP_AB_DEVICE = "ro.build.ab_update"
     private const val PROP_ALLOW_MAJOR_UPGRADES = "lineage.updater.allow_major_upgrades"
     private const val PROP_BUILD_DATE = "ro.build.date.utc"
-    private const val PROP_BUILD_VERSION = "ro.lineage.build.version"
+    private const val PROP_BUILD_VERSION = "ro.witaqua.build.version"
     private const val PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental"
     private const val PROP_DEVICE = "ro.lineage.device"
     private const val PROP_NEXT_DEVICE = "ro.updater.next_device"
-    private const val PROP_RELEASE_TYPE = "ro.lineage.releasetype"
     private const val PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading"
     private const val PROP_UPDATER_URI = "lineage.updater.uri"
     private const val PROP_UPDATE_RECOVERY = "persist.vendor.recovery_update"
@@ -44,9 +43,6 @@ object DeviceInfoUtils : SettingsLibDeviceInfoUtils() {
 
     @JvmStatic
     val isABDevice: Boolean = SystemProperties.getBoolean(PROP_AB_DEVICE, false)
-
-    @JvmStatic
-    val releaseType: String = SystemProperties.get(PROP_RELEASE_TYPE)
 
     // Mutable at runtime
     @JvmStatic
