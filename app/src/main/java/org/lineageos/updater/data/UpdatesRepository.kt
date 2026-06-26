e933d39cf9d3020f37465eff1056e488a3a590dd
Toufu <hamm157famm7423@gmail.com>
2026-06-26 15:24:30 +0000

    Updater: Adapt to WitAqua
    Change-Id: I42a77336895c51d1a15ddf9d012cbd0f6e7caf99

--- app/src/main/java/org/lineageos/updater/data/UpdatesRepository.kt
+++ app/src/main/java/org/lineageos/updater/data/UpdatesRepository.kt
@@ -99,10 +99,6 @@ class UpdatesRepository(
             return false
         }
 
-        if (!update.type.equals(DeviceInfoUtils.releaseType, ignoreCase = true)) {
-            Log.d(TAG, "${update.name} has type ${update.type}")
-            return false
-        }
         return true
     }
 }
